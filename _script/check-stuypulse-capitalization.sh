2336b7a67ef66f58d0faef16cb7efb2422f58c40
Kevin Wang <kevin@kevinwang.com>
2015-02-20 16:36:30 -0500

    Use grep instead of ack.

--- _script/check-stuypulse-capitalization.sh
+++ _script/check-stuypulse-capitalization.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/env bash
 # Checks for instances where "StuyPulse" is incorrectly capitalized as "Stuypulse".
 
-ack=ack
-type ack-grep > /dev/null 2>&1
-if [[ $? -eq 0 ]]; then
-    ack=ack-grep
-fi
-
-$ack --ignore-dir=_site --ignore-dir=_script Stuypulse
+grep -rn --color --exclude-dir=./.git --exclude-dir=./_site --exclude-dir=./_script Stuypulse .
 
 if [[ $? -eq 0 ]]; then
     echo
